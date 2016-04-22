940f396867fa040ed2a6762bd8277b876f39c674
chandrika <chandrika3437@gmail.com>
2016-04-22 14:52:51 +0900

    buffer update

--- buffer.go
+++ buffer.go
@@ -7,11 +7,11 @@ import(
 */
 
 type Buffer struct {
-    content []byte
+    content bytes.Buffer
 }
 
 func (*buf Buffer) set(val string){
-     buf.content := []byte(val)
+     buf.content.Write([]byte(val))
 }
 
 func (*buf Buffer) get() string{
